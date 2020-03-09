269cb1f32351256ac5e65300bcde57fa4f8fd3e0
min <5300min@gmail.com>
2020-03-10 00:58:08 +0900

    posts crud and springsecurity add

--- src/main/java/com/sample/admin/domain/posts/PostsRepository.java
+++ src/main/java/com/sample/admin/domain/posts/PostsRepository.java
@@ -1,10 +1,20 @@
 package com.sample.admin.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 /* DB Layer 접근자
  * JpaRepository<Entity 클래스, PK타입>을 상속하면 기본 CRUD 메소드가 자동 생성
  * Entity 클래스와 함께 위치해야 한다.*/
+/*
+ * JOIN 관련 프레임워크 querydsl
+ * 1. 타입안정성 / 2. 많이 사용함. / 3. 많은 레퍼런스
+ */
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC") /* jpa에서 제공하지 않는 메소드의 경우 쿼리로 작성함 */
+    List<Posts> findAllDesc();
 
 }
--- src/main/java/com/sample/admin/service/posts/PostsService.java
+++ src/main/java/com/sample/admin/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.sample.admin.service.posts;
 
 import com.sample.admin.domain.posts.Posts;
 import com.sample.admin.domain.posts.PostsRepository;
+import com.sample.admin.web.dto.PostsListResponseDto;
 import com.sample.admin.web.dto.PostsResponseDto;
 import com.sample.admin.web.dto.PostsSaveRequestDto;
 import com.sample.admin.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /* 스프링에서 @Autowired 권장 ㄴㄴ
  * 생성자로 Bean 객체를 받도록 함여 동일한 효과
@@ -30,6 +34,19 @@ public class PostsService {
         Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
         posts.update(requestDto.getTitle(), requestDto.getContent());
         return id;
+    }
+
+    @Transactional(readOnly = true) /* 트랜잭션범위유지 -> 조회속도 개선 (등록, 수정, 삭제 없는 메소드에서 사용 추천) */
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new) /* 동일표현 : .map(posts -> new PostsListResponseDto(posts)) / posts 스트림을 map을 통해 List로 변환*/
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다.")); /* 존재하는지 확인 */
+        postsRepository.delete(posts);  /* postsRepository.deleteById(id); => id 또는 엔티티로 삭제가 가능하다. */
     }
 
     public PostsResponseDto findById (Long id) {
